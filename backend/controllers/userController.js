8ccb24474a45829479a39084074c15cdc4de640e
Thomas <tholmes59@gmail.com>
2022-08-25 11:12:00 -0700

    change request to me route to only return id, name, and email of user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,12 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-    res.status(200).json(req.user)
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    }
+    res.status(200).json(user)
 })
 
 // Generate token
