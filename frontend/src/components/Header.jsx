08d466e47c7a073f64bdcfc3c29942e703ee83c2
Thomas <tholmes59@gmail.com>
2022-08-31 08:16:54 -0700

    add logout button and functionality and create conditional to display loggout button when user logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    console.log("click");
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
-    <header className='header'>
-      <div className='logo'>
-          <Link to='/'>Support Desk</Link>
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Support Desk</Link>
       </div>
       <ul>
+        {user ? (
           <li>
-              <Link to='/login'>
-                  <FaSignInAlt/> Login
-                  </Link>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt />
+            </button>
           </li>
-          <li>
-              <Link to='/register'>
-                  <FaUser/> Register
-                  </Link>
-          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
