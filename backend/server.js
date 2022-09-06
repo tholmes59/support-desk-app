0a4c79c92c8e569379d515aec5355d134c1e5541
Thomas <tholmes59@gmail.com>
2022-09-06 14:37:42 -0700

    at tickets route to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const {connectDB} = require('./config/db')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const { connectDB } = require("./config/db");
+const PORT = process.env.PORT || 5000;
 
 //Connect to database
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to Support Desk API'})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Support Desk API" });
+});
 
 //Routes
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
