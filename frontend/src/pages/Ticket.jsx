53f92c502fe87a71f46caa59f5726d19b2468446
Thomas <tholmes59@gmail.com>
2022-11-18 11:53:36 -0800

    build out display for individual ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -22,9 +22,35 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId));
+
+    // eslint-disable-next-line
   }, [isError, message, ticketId]);
 
-  return <div>Ticket</div>;
+  if (isLoading) return <Spinner />;
+
+  if (isError) return <h3>Something went wrong!</h3>;
+
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("en-us")}
+        </h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Ticket;
