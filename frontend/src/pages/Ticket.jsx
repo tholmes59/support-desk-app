0422e3d018e0363c73f28a8b3e8cb99d961db1b9
Thomas <tholmes59@gmail.com>
2022-11-18 09:23:00 -0800

    import components and dependecies, pull in props from redux, and run getTicket function on page load from id in params

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { getTicket, reset } from "../features/tickets/ticketSlice";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 function Ticket() {
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.tickets
+  );
+
+  const params = useParams();
+  const dispatch = useDispatch();
+  const { ticketId } = useParams();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(getTicket(ticketId));
+  }, [isError, message, ticketId]);
+
   return <div>Ticket</div>;
 }
 
